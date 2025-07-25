962d1f910f68ed5b810ca229ab3c99cea7ecca14
Thanh T. Tran <tranthuanthanh535@outlook.com>
2025-07-25 17:17:46 +0700

    feat: add NuGet packages, remove obsolete interface

--- TypedID.Generator/TypedIdGenerator.cs
+++ TypedID.Generator/TypedIdGenerator.cs
@@ -1,38 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
-namespace TypedID.Generator
-{
-    [Generator]
-    public class TypedIdGenerator : ISourceGenerator
-    {
-        public void Initialize(GeneratorInitializationContext context)
-        {
-            // No initialization required for now
-        }
+namespace TypedID.Generator;
 
-        public void Execute(GeneratorExecutionContext context)
+[Generator]
+public class TypedIdGenerator : IIncrementalGenerator
+{
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        // Find all syntax nodes with the TypedIdAttribute
+        var syntaxTrees = context.Compilation.SyntaxTrees;
+        foreach (var tree in syntaxTrees)
         {
-            // Find all syntax nodes with the TypedIdAttribute
-            var syntaxTrees = context.Compilation.SyntaxTrees;
-            foreach (var tree in syntaxTrees)
+            var root = tree.GetRoot();
+            foreach (var classNode in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
             {
-                var root = tree.GetRoot();
-                foreach (var classNode in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
+                foreach (var attributeList in classNode.AttributeLists)
                 {
-                    foreach (var attributeList in classNode.AttributeLists)
+                    foreach (var attribute in attributeList.Attributes)
                     {
-                        foreach (var attribute in attributeList.Attributes)
+                        var name = attribute.Name.ToString();
+                        if (name.Contains("TypedId"))
                         {
-                            var name = attribute.Name.ToString();
-                            if (name.Contains("TypedId"))
-                            {
-                                var idName = classNode.Identifier.Text + "Id";
-                                var source = $@"
+                            var idName = classNode.Identifier.Text + "Id";
+                            var source = $@"
 public readonly record struct {idName}(Guid Value)
 {{
     public static {idName} New() => new(Guid.NewGuid());
@@ -42,8 +41,7 @@ public readonly record struct {idName}(Guid Value)
     public static bool operator !=({idName} left, {idName} right) => left.Value != right.Value;
 }}
 ";
-                                context.AddSource($"{idName}.g.cs", SourceText.From(source, Encoding.UTF8));
-                            }
+                            context.AddSource($"{idName}.g.cs", SourceText.From(source, Encoding.UTF8));
                         }
                     }
                 }
---
+++ TypedID/TypedIdAttribute.cs
@@ -0,0 +1,12 @@
+namespace TypedID;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+public sealed class TypedIdAttribute : Attribute
+{
+    public TypedIdAttribute(Type backingType)
+    {
+        BackingType = backingType;
+    }
+
+    public Type BackingType { get; }
+}
